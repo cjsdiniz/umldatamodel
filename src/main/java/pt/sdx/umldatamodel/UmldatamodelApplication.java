35c8604d2726353def28c396421a6181b1cbe9cb
cjdiniz <cjsdiniz@outlook.com>
2022-01-27 01:01:33 +0000

    First Commit

---
+++ src/main/java/pt/sdx/umldatamodel/UmldatamodelApplication.java
@@ -0,0 +1,13 @@
+package pt.sdx.umldatamodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmldatamodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmldatamodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/sdx/umldatamodel/UmldatamodelApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.sdx.umldatamodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmldatamodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
