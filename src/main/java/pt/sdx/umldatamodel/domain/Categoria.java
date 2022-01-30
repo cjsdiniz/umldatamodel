cd5497f91ef483f96f0f23c2155bbcd0e9613c6d
cjdiniz <cjsdiniz@outlook.com>
2022-01-30 16:31:14 +0000

    Categoria upd

--- src/main/java/pt/sdx/umldatamodel/domain/Categoria.java
+++ src/main/java/pt/sdx/umldatamodel/domain/Categoria.java
@@ -1,9 +1,18 @@
 package pt.sdx.umldatamodel.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // import java.io.Serializable;
 
+@Entity
 // public class Categoria implements Serializable {
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
